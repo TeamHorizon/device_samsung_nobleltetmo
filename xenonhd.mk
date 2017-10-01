236aa3c0f779c3a64ddd63c1cba1eddf6ba89a49
ljjehl <ljjehl@gmail.com>
2017-10-01 18:52:06 +0200

    Welcome to Xenonhd

--- xenonhd.mk
+++ xenonhd.mk
@@ -9,10 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_nobleltetmo
+PRODUCT_NAME := xenonhd_nobleltetmo
 PRODUCT_DEVICE := nobleltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N920T
